9d329bb6ee51493cf0847bf265876f4004030bd2
Dale Stevens <dale@twilightcoders.net>
2025-07-16 00:45:20 -0600

    Add main models module with convenience methods
    - Central module for model loading and management
    - Convenience methods for creating models from JSON
    - Integration point for all model components

---
+++ lib/pandoru/models.rb
@@ -0,0 +1,18 @@
+require_relative 'models/_base'
+require_relative 'models/station'
+require_relative 'models/playlist'
+require_relative 'models/search'
+require_relative 'models/bookmark'
+
+module Pandoru
+  module Models
+    # Convenience method to create models from API responses
+    def self.from_json(model_class, data, api_client = nil)
+      model_class.from_json(data, api_client)
+    end
+
+    def self.from_json_list(model_class, data_list, api_client = nil)
+      model_class.from_json_list(data_list, api_client)
+    end
+  end
+end
